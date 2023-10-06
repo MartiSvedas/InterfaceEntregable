b3c6706897ccdce3e34a81feeabc12ee7913d1a7
MartiSvedas <90530390+MartiSvedas@users.noreply.github.com>
2023-10-06 17:00:51 -0300

    carrusel funcional

--- tp2/js/carrusel.js
+++ tp2/js/carrusel.js
@@ -1,16 +1,17 @@
 const btnright = document.getElementById("button-next")
 const btnleft = document.getElementById("button-prev")
-const slider = document.querySelector("#carrusel")
-const slidersection = document.querySelectorAll(".carruselLi")
-const elemento= slidersection[0].offsetWidth;
 
-btnright.addEventListener("click", ()=>{
-    
+const slider = document.querySelector("#carrusel")
+
+const slidersection = document.querySelector(".carruselLi")
+
+
+btnright.addEventListener('click', ()=>{
+    slider.scrollLeft += slider.offsetWidth;
 
 })
-btnleft.addEventListener("click", ()=>{    
+btnleft.addEventListener('click', ()=>{    
     
-    elemento.style.transform ="translate(33.3%)"
-    
+    slider.scrollLeft = slider.offsetWidth;
 
 })
