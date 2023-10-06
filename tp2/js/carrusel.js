18b3bc9fe7c0a4ec9393a4e6d5b23fffd3a8fd6a
MartiSvedas <90530390+MartiSvedas@users.noreply.github.com>
2023-10-06 15:57:53 -0300

    cambios Co-authored-by: ValentinaHurtado <ValentinaHurtado@users.noreply.github.com>

---
+++ tp2/js/carrusel.js
@@ -0,0 +1,16 @@
+const btnright = document.getElementById("button-next")
+const btnleft = document.getElementById("button-prev")
+const slider = document.querySelector("#carrusel")
+const slidersection = document.querySelectorAll(".carruselLi")
+const elemento= slidersection[0].offsetWidth;
+
+btnright.addEventListener("click", ()=>{
+    
+
+})
+btnleft.addEventListener("click", ()=>{    
+    
+    elemento.style.transform ="translate(33.3%)"
+    
+
+})
