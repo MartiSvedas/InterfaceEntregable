6aaaa3dec8ab0196420e1dc32ac673e71429747d
Valentina Hurtado <vhurtado@alumnos.exa.unicen.edu.ar>
2023-11-22 20:27:25 -0300

    cambio

--- tp4/js/cards.js
+++ tp4/js/cards.js
@@ -1,12 +1,12 @@
 var elScroll = document.querySelectorAll('.cardsJuegoSM');
 
-document.onscroll = function() { //ao rolar a tela...
+document.onscroll = function() { 
     elScroll.forEach(elScroll => {
-    var positionEl = elScroll.getBoundingClientRect(); //pega valores da posição do elemento
-    var alturaEl = positionEl.top; //pega distancia do topo da tela
+    var positionEl = elScroll.getBoundingClientRect(); 
+    var alturaEl = positionEl.top; 
   
-    if(alturaEl < 300) { //se a distancia do topo for menor que 300
-        elScroll.classList.add('active'); //adiciona a classe .scroll--show
+    if(alturaEl < 500) { 
+        elScroll.classList.add('active'); 
     }
 });
 }
