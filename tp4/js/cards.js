6c1891865e8240d9bfe7a904c5eac58a6bbdc297
Valentina Hurtado <vhurtado@alumnos.exa.unicen.edu.ar>
2023-11-22 20:41:34 -0300

    cambio fade in

--- tp4/js/cards.js
+++ tp4/js/cards.js
@@ -1,12 +1,19 @@
 var elScroll = document.querySelectorAll('.cardsJuegoSM');
+let time = -1;
 
 document.onscroll = function() { 
     elScroll.forEach(elScroll => {
-    var positionEl = elScroll.getBoundingClientRect(); 
-    var alturaEl = positionEl.top; 
-  
-    if(alturaEl < 500) { 
-        elScroll.classList.add('active'); 
+    time+=1;
+    setTimeout(function(){
+        var positionEl = elScroll.getBoundingClientRect(); 
+        var alturaEl = positionEl.top; 
+      
+        if(alturaEl < 500) { 
+            elScroll.classList.add('active'); 
+        }
     }
+        
+    ,time*1000);
+    
 });
 }
