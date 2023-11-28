cd742feeae90f9b146aa2e6c640e1b8a68783131
MartiSvedas <90530390+MartiSvedas@users.noreply.github.com>
2023-11-28 16:52:20 -0300

    logo

--- tp4/js/header.js
+++ tp4/js/header.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {
     let header = document.querySelector('.fondo');
     let logo = document.querySelector('.logo');
-    let logoPequeño=document.querySelector('.logoPequeño')
+    // let logoPequeño=document.querySelector('.logoPequeño')
     let menuHamburguesa = document.querySelector('.btn_menu');
     let button = document.querySelector('.btn_comprar');
     if (window.pageYOffset > 70) { 
@@ -10,18 +10,17 @@ window.onscroll = function() {
         menuHamburguesa.style.transform='scale(0.9)';
         button.style.transform= 'scale(0.8)';
         button.style.top='-4px';
-        logo.style.transform = 'scale(0.0)';
-        logoPequeño.classList.add('mostrard');
+        logo.style.transform = 'scale(0.3)';
+        logo.style.top='-196px';
+        logo.style.position='fixed'
     }else {
         header.style.height='';
         menuHamburguesa.style.top= '';
         menuHamburguesa.style.transform='';
         logo.style.transform = 'scale(1)';
-        logo.style.position='absolute';
-        logo.style.marginTop=''
+        logo.style.top='';
         button.style.transform= '';
         button.style.top='';
-        logoPequeño.classList.remove('mostrard');
     }
   };
 
