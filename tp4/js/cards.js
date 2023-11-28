0a67a2212b7f7cd5ecf69ede39006b5062cec972
Valentina Hurtado <vhurtado@alumnos.exa.unicen.edu.ar>
2023-11-28 19:14:01 -0300

    comento funciones

--- tp4/js/cards.js
+++ tp4/js/cards.js
@@ -1,11 +1,16 @@
+//selecciono las cards y las guardo en una variable
 var cards = document.querySelectorAll('.cardsJuegoSM');
-
+//agrego funcion al hacer scroll
 document.addEventListener('scroll', function() {
+    //para cada card paso como parametro a esta misma y el indice del arr (q seria el nro q corresponde dependiendo de la card)
     cards.forEach((card, index) => {
+        //creo variable que tiene la posicion de la card
         var pos = card.getBoundingClientRect();
-        var altura = pos.top;
-        if (altura < 550) {
+        var altura = pos.top; //creo variable con la posicion superior de la card
+        if (altura < 550) { //mientras q la pos este a menos de 550px de la parte de arriba
+            //le agrego un transitionDelay a la card, q depende del nro de card q es
             card.style.transitionDelay = index * 0.5 + 's';
+            //le agrego clase active para agregarles los estilos q corresponden
             card.classList.add('active');
         }
     });
--- tp4/js/parallaxSeccionGS.js
+++ tp4/js/parallaxSeccionGS.js
@@ -1,13 +1,16 @@
 document.addEventListener('scroll', function() {
+    //llamo a la seccion donde se encuentran los cards
     let seccion = document.querySelector('.seccionGwenStacy');
+    //creo una variable con el valor en Y de la seccion en la parte superior de la pantalla
     const scrollPosition = seccion.getBoundingClientRect().top;
-
-    // Parallax effect
+    console.log(scrollPosition);
+    //llamo a las cards q van a tener el efecto con su clase
     const parallaxElements = document.querySelectorAll('.parallax-elementGS');
     parallaxElements.forEach(element => {
+        //creo variable speed con la velocidad asignada en cada card
         const speed = element.getAttribute('data-speed');
-        const translateY = (scrollPosition * speed/100);
-        console.log(translateY);
+        //creo variable que va a transformar la posicion en Y de la card
+        const translateY = (scrollPosition * speed/100); 
         element.setAttribute('style', 'transform: translateY( ' + translateY + 'px)');
     });
 });
